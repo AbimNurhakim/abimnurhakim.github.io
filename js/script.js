a5472a4959b3d03030f6e3dc98aef7e1d0846b1f
abimnurhakim <abimnurhakim@gmail.com>
2024-07-27 17:03:24 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('.page-scroll').on('click', function () {
+  var tujuan = $(this).attr('href');
+  var elemenTujuan = $(tujuan);
+  $('html,body').animate(
+    {
+      scrollTop: elemenTujuan.offset().top - 100,
+    },
+    1000,
+    'swing'
+  );
+});
